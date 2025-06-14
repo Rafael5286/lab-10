e02911b217ca96f5fe308cce6fda7d26c1dff191
Rafael5286 <rafael.slv204@gmail.com>
2025-06-13 21:05:53 -0300

    Add files via upload

---
+++ controller/Main.java
@@ -0,0 +1,22 @@
+package controller;
+
+import model.*;
+import view.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Onibus onibus = new Onibus(3);
+
+        PainelCentral painel = new PainelCentral();
+        Quiosque q1 = new Quiosque("A");
+        Quiosque q2 = new Quiosque("B");
+
+        onibus.adicionarObserver(painel);
+        onibus.adicionarObserver(q1);
+        onibus.adicionarObserver(q2);
+
+        onibus.reservarAssento(2);
+        onibus.indisponibilizarAssento(3);
+    }
+}
